8e947cb11bdc2dbdcda443c90a7dc0c98b130f63
tm4br <80352121+tm4br@users.noreply.github.com>
2022-06-17 11:40:35 +0200

    Update Ball and EV3.py

--- PC/Ball and EV3.py
+++ PC/Ball and EV3.py
@@ -8,25 +8,25 @@ cap = cv2.VideoCapture("rtsp://141.46.137.93:8554/mystream")
 # Bitrate 5000000
 
 #initialize green ball
-green = cv2.imread('GreenBall.jpg')
+green = cv2.imread('GreenBall.png')
 wGreen = green.shape[0]
 hGreen = green.shape[1]
 
 cv2.imshow("TemplateGreen", green)
 
 #initialize red ball
-red = cv2.imread('redBall.jpg')
+red = cv2.imread('RedBall.png')
 wRed = red.shape[0]
 hRed = red.shape[1]
 
 cv2.imshow("TemplateRed", red)
 
-#initialize black ball
-black = cv2.imread('black ball.png')
-wBlack = black.shape[0]
-hBlack = black.shape[1]
+#initialize yellow ball
+yellow = cv2.imread('YellowBall.png')
+wYellow = yellow.shape[0]
+hYellow = yellow.shape[1]
 
-cv2.imshow("TemplateBlack", black)
+cv2.imshow("TemplateBlack", yellow)
 
 if (cap.isOpened()):
     print("Capturing ...")
@@ -42,8 +42,8 @@ while(cap.isOpened()):
     green_min_val, green_max_val, green_min_loc, green_max_loc = cv2.minMaxLoc(resGreen)
 
     green_top_left = green_max_loc
-    green_bottom_right = (green_top_left[0] + 25 , green_top_left[1] + 25)
-    green_top_left_final = (green_top_left[0] + 25, green_top_left[1] + 25)
+    green_bottom_right = (green_top_left[0] + 17 , green_top_left[1] + 17)
+    green_top_left_final = (green_top_left[0] + 17, green_top_left[1] + 17)
 
 #Koordinaten der Mitte des Balls
     print("Green Ball: ", green_top_left_final)
@@ -57,8 +57,8 @@ while(cap.isOpened()):
     red_min_val, red_max_val, red_min_loc, red_max_loc = cv2.minMaxLoc(resRed)
 
     red_top_left = red_max_loc
-    red_bottom_right = (red_top_left[0] + 31, red_top_left[1] + 41)
-    red_top_left_final = (red_top_left[0] + 31, red_top_left[1] + 41)
+    red_bottom_right = (red_top_left[0] + 15, red_top_left[1] + 12)
+    red_top_left_final = (red_top_left[0] + 15, red_top_left[1] + 12)
 
     # Koordinaten der Mitte des Balls
     print("Red Ball: ", red_top_left_final)
@@ -66,19 +66,19 @@ while(cap.isOpened()):
     cv2.rectangle(frame, red_top_left_final, red_bottom_right, 255, 2)
 
 
-    # black ball tracking
+    # yellow ball tracking
 
-    resBlack = cv2.matchTemplate(frame, black, 5)
-    black_min_val, black_max_val, black_min_loc, black_max_loc = cv2.minMaxLoc(resBlack)
+    resYellow = cv2.matchTemplate(frame, yellow, 5)
+    yellow_min_val, yellow_max_val, yellow_min_loc, yellow_max_loc = cv2.minMaxLoc(resYellow)
 
-    black_top_left = black_max_loc
-    black_bottom_right = (black_top_left[0] + 15, black_top_left[1] + 15)
-    black_top_left_final = (black_top_left[0] + 15, black_top_left[1] + 15)
+    yellow_top_left = yellow_max_loc
+    yellow_bottom_right = (yellow_top_left[0] + 20, yellow_top_left[1] + 20)
+    yellow_top_left_final = (yellow_top_left[0] + 20, yellow_top_left[1] + 20)
 
     # Koordinaten der Mitte des Balls
-    print("Black Ball: ", black_top_left_final)
+    print("Yellow Ball: ", yellow_top_left_final)
 
-    cv2.rectangle(frame, black_top_left_final, black_bottom_right, 255, 2)
+    cv2.rectangle(frame, yellow_top_left_final, yellow_bottom_right, 255, 2)
 
     cv2.imshow("Video", frame)  # Anzeige des Videoframes
 
