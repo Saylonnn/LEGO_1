0d0dee60f773a06c3128ac83c9cca1ef41066ea1
tm4br <80352121+tm4br@users.noreply.github.com>
2022-06-29 09:24:11 +0200

    Update ball_ev3.py

--- PC/ball_ev3.py
+++ PC/ball_ev3.py
@@ -4,6 +4,8 @@ from pyueye import ueye
 import threading
 import socket
 import numpy as np
+import sys
+import pickle
 
 
 class ConnectionThread(threading.Thread):
@@ -20,7 +22,8 @@ class ConnectionThread(threading.Thread):
                 break
 
     def send(self, corList):
-        self.conn.sendall(bytes(corList))
+        self.conn.sendall(corList)
+
 
 class ThreadingServer():
     def __init__(self):
@@ -28,13 +31,14 @@ class ThreadingServer():
         self.port = 8485
         self.connections = []
         self.ip = socket.gethostbyname(socket.gethostname())
+
     def run(self):
-        server_thread = threading.Thread(target= self.run_server)
+        server_thread = threading.Thread(target=self.run_server)
         print("server thread started")
-        cam_thread = threading.Thread(target= self.run_cam)
+        cam_thread = threading.Thread(target=self.run_cam)
         server_thread.start()
         cam_thread.start()
-        
+
     def run_server(self):
         print("run server thread")
         self.sock.bind((self.ip, self.port))
@@ -49,43 +53,41 @@ class ThreadingServer():
             print(len(self.connections))
 
     def run_cam(self):
-        #Variables
-        hCam = ueye.HIDS(0)             #0: first available camera;  1-254: The camera with the specified camera ID
+        # Variables
+        hCam = ueye.HIDS(0)  # 0: first available camera;  1-254: The camera with the specified camera ID
         sInfo = ueye.SENSORINFO()
         cInfo = ueye.CAMINFO()
         pcImageMemory = ueye.c_mem_p()
         MemID = ueye.int()
         rectAOI = ueye.IS_RECT()
         pitch = ueye.INT()
-        nBitsPerPixel = ueye.INT(24)    #24: bits per pixel for color mode; take 8 bits per pixel for monochrome
-        channels = 3                    #3: channels for color mode(RGB); take 1 channel for monochrome
-        m_nColorMode = ueye.INT()		# Y8/RGB16/RGB24/REG32
+        nBitsPerPixel = ueye.INT(24)  # 24: bits per pixel for color mode; take 8 bits per pixel for monochrome
+        channels = 3  # 3: channels for color mode(RGB); take 1 channel for monochrome
+        m_nColorMode = ueye.INT()  # Y8/RGB16/RGB24/REG32
         bytes_per_pixel = int(nBitsPerPixel / 8)
         pParam = ueye.wchar_p()
-        
-        
-        
-        #cap = cv2.VideoCapture("ball.avi")
-        #cap = cv2.VideoCapture("rtsp://141.46.137.93:8554/mystream")
+
+        # cap = cv2.VideoCapture("ball.avi")
+        # cap = cv2.VideoCapture("rtsp://141.46.137.93:8554/mystream")
         # ueye_streamer
         # Rtsp port 8554
         # 1024 x 768 15 fps
         # Bitrate 5000000
 
-        #initialize green ball
-        green = cv2.imread('GreenBall.png')
+        # initialize green ball
+        green_1 = cv2.imread('C:\\Users\\User\\PycharmProjects\\Bildverarbeitung\\EV3\\Ball and EV3\\EV3 Ball and EV3\\GreenBall.png', -1)
 
-        #initialize red ball
-        red = cv2.imread('RedBall.png')
+        # initialize red ball
+        red = cv2.imread('RedBall.png', -1)
 
-        #initialize yellow ball
-        yellow = cv2.imread('YellowBall.png')
+        # initialize yellow ball
+        yellow = cv2.imread('YellowBall.png', -1)
 
-        #initialize blue ball
-        blue = cv2.imread('BlueBall.png')
+        # initialize blue ball
+        blue = cv2.imread('BlueBall.png', -1)
 
-        #initialize black ball
-        black = cv2.imread('BlackBall.png')
+        # initialize black ball
+        black = cv2.imread('BlackBall.png', -1)
 
         '''
         if (cap.isOpened()):
@@ -102,7 +104,6 @@ class ThreadingServer():
         print("START")
         print()
 
-
         # Starts the driver and establishes the connection to the camera
         nRet = ueye.is_InitCamera(hCam, None)
         if nRet != ueye.IS_SUCCESS:
@@ -118,7 +119,7 @@ class ThreadingServer():
         if nRet != ueye.IS_SUCCESS:
             print("is_GetSensorInfo ERROR")
 
-        nRet = ueye.is_ResetToDefault( hCam)
+        nRet = ueye.is_ResetToDefault(hCam)
         if nRet != ueye.IS_SUCCESS:
             print("is_ResetToDefault ERROR")
 
@@ -174,7 +175,6 @@ class ThreadingServer():
 
         height = rectAOI.s32Height
 
-
         # Prints out some information about the camera and the sensor
         print("Camera model:\t\t", sInfo.strSensorName.decode('utf-8'))
         print("Camera serial no.:\t", cInfo.SerNo.decode('utf-8'))
@@ -182,7 +182,7 @@ class ThreadingServer():
         print("Maximum image height:\t", height)
         print()
 
-        #---------------------------------------------------------------------------------------------------------------------------------------
+        # ---------------------------------------------------------------------------------------------------------------------------------------
         pParam.value = "1.ini"
 
         ueye.is_ParameterSet(hCam, ueye.IS_PARAMETERSET_CMD_LOAD_FILE, pParam, 0)
@@ -200,8 +200,6 @@ class ThreadingServer():
                 # Set the desired color mode
                 nRet = ueye.is_SetColorMode(hCam, m_nColorMode)
 
-
-
         # Activates the camera's live video mode (free run mode)
         nRet = ueye.is_CaptureVideo(hCam, ueye.IS_DONT_WAIT)
         if nRet != ueye.IS_SUCCESS:
@@ -215,7 +213,7 @@ class ThreadingServer():
             print("Press q to leave the programm")
 
         # Continuous image display
-        while(nRet == ueye.IS_SUCCESS):
+        while (nRet == ueye.IS_SUCCESS):
 
             # In order to display the image in an OpenCV window we need to...
             # ...extract the data of our image memory
@@ -224,30 +222,25 @@ class ThreadingServer():
             # bytes_per_pixel = int(nBitsPerPixel / 8)
 
             # ...reshape it in an numpy array...
-            frame = np.reshape(array,(height.value, width.value, bytes_per_pixel))
+            frame = np.reshape(array, (height.value, width.value, bytes_per_pixel))
 
             # ...resize the image by a half
-            #frame = cv2.resize(frame,(0,0),fx=0.5, fy=0.5)
-
-    
-
-
-
-            #green ball tracking
-            resGreen = cv2.matchTemplate(frame, green, 5)
+            # frame = cv2.resize(frame,(0,0),fx=0.5, fy=0.5)
+            #cv2.imshow('frame', frame)
+            # green ball tracking
+            resGreen = cv2.matchTemplate(frame, green_1, 5)
             green_min_val, green_max_val, green_min_loc, green_max_loc = cv2.minMaxLoc(resGreen)
 
             green_top_left = green_max_loc
-            green_bottom_right = (green_top_left[0] + 17 , green_top_left[1] + 17)
+            green_bottom_right = (green_top_left[0] + 17, green_top_left[1] + 17)
             green_top_left_final = (green_top_left[0] + 17, green_top_left[1] + 17)
 
-        #Koordinaten der Mitte des Balls
+            # Koordinaten der Mitte des Balls
             print("Green Ball: ", green_top_left_final)
 
             cv2.rectangle(frame, green_top_left_final, green_bottom_right, 255, 2)
 
-
-            #red ball tracking
+            # red ball tracking
 
             resRed = cv2.matchTemplate(frame, red, 5)
             red_min_val, red_max_val, red_min_loc, red_max_loc = cv2.minMaxLoc(resRed)
@@ -259,17 +252,17 @@ class ThreadingServer():
             # Koordinaten der Mitte des Balls
             print("Red Ball: ", red_top_left_final)
 
-            #TODO: wenn Ball im Feld, dann hinfahren
-            #TODO: wenn Ball außerhalb vom Feld, dann zum Tor fahren und schießen
-            #TODO: dafür alle Ecken ausmessen und roten Ball außerhalb vom Feld legen
+            # TODO: wenn Ball im Feld, dann hinfahren
+            # TODO: wenn Ball außerhalb vom Feld, dann zum Tor fahren und schießen
+            # TODO: dafür alle Ecken ausmessen und roten Ball außerhalb vom Feld legen
 
-            if red_top_left_final[0] > 56 & red_top_left_final[0] < 908 & red_top_left_final[1] > 105 & red_top_left_final[1] < 638:
+            if red_top_left_final[0] > 56 & red_top_left_final[0] < 908 & red_top_left_final[1] > 105 & \
+                    red_top_left_final[1] < 638:
                 print("red ball inside")
             else:
                 print("red ball outside")
 
             cv2.rectangle(frame, red_top_left_final, red_bottom_right, 255, 2)
-
 
             # yellow ball tracking
 
@@ -312,24 +305,37 @@ class ThreadingServer():
             print("Blue Ball: ", blue_top_left_final)
 
             cv2.rectangle(frame, blue_top_left_final, blue_bottom_right, 255, 2)
-            cv2.rectangle(frame, (56, 105), (908, 638), 255,2)
+            cv2.rectangle(frame, (56, 105), (908, 638), 255, 2)
 
-            cords = [red_top_left_final, black_top_left_final, blue_top_left_final, yellow_top_left_final, green_top_left_final, [57,286], [57, 461], [947, 283], [947, 458], True]
-            
-            
+            #cords = [red_top_left_final[0], red_top_left_final[1], black_top_left_final[0], black_top_left_final[1], blue_top_left_final[0], blue_top_left_final[1], yellow_top_left_final[0], yellow_top_left_final[1],
+            #       green_top_left_final[0], green_top_left_final[1], 57, 286, 57, 461, 947, 283, 947, 458, True]
+            cords_1 = [str(red_top_left_final[0]),",", str(red_top_left_final[1]),",", str(black_top_left_final[0]),",", str(black_top_left_final[1]),",", str(blue_top_left_final[0]),",", str(blue_top_left_final[1]),",", str(yellow_top_left_final[0]),",", str(yellow_top_left_final[1]),",",
+                               str(green_top_left_final[0]),",", str(green_top_left_final[1]), ",",str(57),",", str(286),",", str(57),",", str(461), ",",str(947), ",",str(283),",", str(947),",",str(458),",", str(True)]
+
+            s = ''.join(cords_1)
+            print(s)
+
+            #data = pickle.dumps(cords)
+
+           #cords = [red_top_left_final[0],",", red_top_left_final[1],",", black_top_left_final[0],",", black_top_left_final[1],",", blue_top_left_final[0],",", blue_top_left_final[1],",", yellow_top_left_final[0],",", yellow_top_left_final[1],",",
+            #         green_top_left_final[0],",", green_top_left_final[1],",", 57,",", 286,",", 57,",", 461,",", 947,",", 283,",", 947,",", 458,",", True]
+
+
+            '''cords = [red_top_left_final, black_top_left_final, blue_top_left_final, yellow_top_left_final,
+                     green_top_left_final, [57, 286], [57, 461], [947, 283], [947, 458], True]'''
             for x in self.connections:
                 try:
-                    x.send(b'cords')
+                    x.send(str.encode(s))
                 except:
                     print("connection closed")
                     self.connections.remove(x)
-            #cv2.imshow("Video", frame)  # Anzeige des Videoframes
+            # cv2.imshow("Video", frame)  # Anzeige des Videoframes
 
-            
         ueye.is_FreeImageMem(hCam, pcImageMemory, MemID)
         ueye.is_ExitCamera(hCam)
-        #cap.release()
-        cv2.destroyAllWindows() # Close all windows
+        # cap.release()
+        cv2.destroyAllWindows()  # Close all windows
+
 
 ts = ThreadingServer()
 ts.run()
