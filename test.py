3673e7eb80a9dd03c25e5a10b58de6c21ce3ced9
Saylonnn <85989616+Saylonnn@users.noreply.github.com>
2022-07-04 09:01:07 +0200

    test.py

--- test.py
+++ test.py
@@ -1,118 +1,39 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+#link https://stackoverflow.com/questions/56875876/detect-circles-with-specific-colors-using-opencv
 
-Matrix1 = [[6,5,4,3,2,1,2,3],
-            [5,4,3,2,1,0,1,2],
-            [6,5,4,3,2,1,2,3],
-            [7,6,5,4,3,2,3,4],
-            [8,7,6,5,4,3,4,5],
-            [9,8,7,6,5,4,5,6],
-            [10,9,8,7,6,5,6,7],
-            [11,10,9,8,7,6,7,8],
-            [12,11,10,9,8,7,8,9],
-            [13,12,11,10,9,8,9,10],
-            [14,13,12,11,10,9,10,11]]
+cap = cv.VideoCapture('1.mp4')
+template_green = cv.imread('PC\PatternPics\GreenBall.jpg', -1)
+cv.imshow('Green Template', template_green)
 
+if (cap.isOpened() == False):
+    print("Error opening Video stream or file.")
 
-'''
-6 5 4 3 2 1 2 3
-5 4 3 2 1 0 1 2
-6 5 4 3 2 1 2 3
-7 6 5 4 3 2 3 4
-8 7 6 5 4 3 4 5
-9 8 7 6 5 4 5 6
-10 9 8 7 6 5 6 7
-11 10 9 8 7 6 7 8
-12 11 10 9 8 7 8 9
-13 12 11 10 9 8 9 10
-14 13 12 11 10 9 10 11
-'''
-
-
-engine_left = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
-engine_right = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        '''
+        result = cv.matchTemplate(frame, template_green, cv.TM_CCORR_NORMED)
+        (minVal, maxVal, minLoc, maxLoc)= cv.minMaxLoc(result)
+        top_left = minLoc
+        bottom_right = (top_left[0] + 50, top_left[1] + 50)
         
+        cv.rectangle(frame,top_left, bottom_right, (255, 0, 0), 3)
+        '''
 
+        lower_blue = np.array([80, 60, 30])
+        upper_blue = np.array([100, 90, 50])
 
+        mask = cv.inRange(frame, lower_blue, upper_blue)
+        res = cv.bitwise_and(frame, frame, mask=mask)
 
-class Bahnplanung:
-    def __init__(self, matrix, start_x, start_y):
-        self.direction = 'FW'
-        self.matrix = matrix
-        self.start_x = start_x
-        self.start_y = start_y
-      
-        self.engine_speed = 200
-        self.neg_engine_speed = 0 - self.engine_speed
-        self.field_length = 200
-        self.drive_time_to_next_field = self.field_length / 0.0735
-
-    
-    def find_path(self):
-        print("pathfindng start")
-        x = self.start_x
-        y = self.start_y
-        value = self.matrix[x][y]
-        value_front = 1000
-        value_right = 1000
-        value_left = 1000
-        value_back = 1000
-        #value_list = [value_front, value_right, value_back, value_left]
-        while value != 0:
-            print("field [",x,"][",y,"]")
-            try:
-                value_front = matrix[x][y-1]
-            except:
-                value_front = 1000
-            try:
-                value_right = matrix[x+1][y]
-            except:
-                value_right = 1000
-            try:
-                value_left = matrix[x-1][y]
-            except:
-                value_left = 1000
-            try:
-                value_back = matrix[x][y+1]
-            except:
-                value_back = 1000
-
-            lowest = 1000
-            lowest_direction = None
-            if value_front < lowest:
-                lowest = value_front
-                lowest_direction = 'FW'
-            if value_right < lowest:
-                lowest = value_right
-                lowest_direction = 'R'
-            if value_back < lowest:
-                lowest = value_back
-                lowest_direction = 'BW'
-            if value_left < lowest:
-                lowest = value_left
-                lowest_direction = 'L'
-            match lowest_direction:
-                case 'FW':
-                    self.go_FW()
-                    y = y-1
-                case 'R':
-                    self.go_right()
-                    x = x+1
-                case 'L':
-                    self.go_left()
-                    x = x-1
-                case 'BW':
-                    self.go_back()
-                    x = y+1
-            
-
-
-    def go_FW(self):
-        print("go_FW")
-
-    def go_right(self):
-        print("go_right")
-    
-    def go_left(self):
-        print("go_left")
-    
-    def go_back(self):
-        print("go_back")
+        cv.imshow('mask', mask)
+        cv.imshow('Frame', frame)
+        cv.imshow('res', res)
+        if cv.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        break
